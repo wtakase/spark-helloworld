3cfd4888955ed2f642e4ba46601eabcda1f77d67
Wataru Takase <wataru.takase@kek.jp>
2017-04-04 11:39:42 +0900

    Add SparkContext line

--- src/main/scala/jp/kek/spark/helloworld/HelloWorld.scala
+++ src/main/scala/jp/kek/spark/helloworld/HelloWorld.scala
@@ -1,6 +1,11 @@
 package jp.kek.spark.helloworld
 
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+
 object HelloWorld {
-  def main(args: Array[String]):
-    Unit = println("Hello World!")
+  def main(args: Array[String]): Unit = {
+    val sc = new SparkContext(new SparkConf().setAppName("Hello World"))
+    println("Hello World!")
+  }
 }
